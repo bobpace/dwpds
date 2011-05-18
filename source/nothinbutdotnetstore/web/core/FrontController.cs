85d0cbe6f46e86e5c26b76c332b8c5cca2d39513
Bob Pace <bob.pace@gmail.com>
2011-05-18 10:39:36 -0600

    Pushing new changes

--- source/nothinbutdotnetstore/web/core/FrontController.cs
+++ source/nothinbutdotnetstore/web/core/FrontController.cs
@@ -4,9 +4,17 @@ namespace nothinbutdotnetstore.web.core
 {
   public class FrontController : IProcessIncomingRequests
   {
+    readonly IFindCommands find_commands;
+
+    public FrontController(IFindCommands find_commands)
+    {
+      this.find_commands = find_commands;
+    }
+
     public void process(IContainRequestInformation request)
     {
-      throw new NotImplementedException();
+      var command = find_commands.get_the_command_that_can_process(request);
+      command.run(request);
     }
   }
 }
