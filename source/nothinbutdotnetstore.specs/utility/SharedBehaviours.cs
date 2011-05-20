8f3b6feb15dc8294044cd017cd003cd182c7375a
Bob Pace <bob.pace@gmail.com>
2011-05-20 17:08:48 -0600

    Pushing new changes

--- source/nothinbutdotnetstore.specs/utility/SharedBehaviours.cs
+++ source/nothinbutdotnetstore.specs/utility/SharedBehaviours.cs
@@ -1,5 +1,7 @@
 using System;
 using developwithpassion.specifications;
+using nothinbutdotnetstore.infrastructure.container;
+using nothinbutdotnetstore.infrastructure.container.stubs;
 
 namespace nothinbutdotnetstore.specs.utility
 {
@@ -7,7 +9,13 @@ namespace nothinbutdotnetstore.specs.utility
   {
     public static ObservationPair scaffold_container_returned<Dependency>(Dependency dependency)
     {
-      throw new NotImplementedException();
+      return new ObservationPair(() =>
+      {
+        Container.gateway_resolver = () => new StubContainer();
+      }, () =>
+      {
+
+      });
     }
   }
 }
